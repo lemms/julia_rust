f9fb4c40095071fc59dd2b6523d783b77e4b8f7e
Laurence Emms <laurenceemms@gmail.com>
2022-05-07 14:46:12 -0700

    Added CIVIDIS

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use image::ImageBuffer;
 
+mod cividis;
 mod julia;
 
 fn write_image(filename: &str, w: u32, h: u32) {
@@ -12,7 +13,10 @@ fn write_image(filename: &str, w: u32, h: u32) {
 
     let image = ImageBuffer::from_fn(w, h, |x, y| {
         let i = julia::julia(x as f32, y as f32, w as f32, h as f32, move_x, move_y, zoom, cx, cy, max_iter);
-        image::Rgb([(i << 21) as u8, (i << 10) as u8, (i << 3) as u8])
+        let index = (i as f32 / max_iter as f32) as usize;
+        image::Rgb([(cividis::CIVIDIS[index][0] * 255.0) as u8,
+                    (cividis::CIVIDIS[index][1] * 255.0) as u8,
+                    (cividis::CIVIDIS[index][2] * 255.0) as u8])
     });
 
     image.save(filename).unwrap();
