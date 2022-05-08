59445715ec057e814c83298893c1c5686753d0bc
Laurence Emms <laurenceemms@gmail.com>
2022-05-08 00:45:29 -0700

    Added color maps.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use image::ImageBuffer;
 
-mod cividis;
+//mod cividis;
+//mod inferno;
 mod julia;
+//mod magma;
+//mod plasma;
+mod turbo;
+//mod twilight;
+//mod viridis;
 
 fn write_image(filename: &str, w: u32, h: u32) {
     let move_x = 0.0;
@@ -9,14 +15,14 @@ fn write_image(filename: &str, w: u32, h: u32) {
     let zoom = 1.0;
     let cx = -0.7;
     let cy = 0.27015;
-    let max_iter = 255;
+    let max_iter = 1024;
 
     let image = ImageBuffer::from_fn(w, h, |x, y| {
         let i = julia::julia(x as f32, y as f32, w as f32, h as f32, move_x, move_y, zoom, cx, cy, max_iter);
-        let index = (255.0 - i as f32 / max_iter as f32 * 255.0) as usize;
-        image::Rgb([(cividis::CIVIDIS[index][0] * 255.0) as u8,
-                    (cividis::CIVIDIS[index][1] * 255.0) as u8,
-                    (cividis::CIVIDIS[index][2] * 255.0) as u8])
+        let index = (i as f32 / max_iter as f32 * 255.0) as usize;
+        image::Rgb([(turbo::TURBO[index][0] * 255.0) as u8,
+                    (turbo::TURBO[index][1] * 255.0) as u8,
+                    (turbo::TURBO[index][2] * 255.0) as u8])
     });
 
     image.save(filename).unwrap();
