76d5aec0f23a490066550b920287ceb7c93808df
Laurence Emms <laurenceemms@gmail.com>
2022-05-07 21:32:20 -0700

    Changed coloration

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn write_image(filename: &str, w: u32, h: u32) {
 
     let image = ImageBuffer::from_fn(w, h, |x, y| {
         let i = julia::julia(x as f32, y as f32, w as f32, h as f32, move_x, move_y, zoom, cx, cy, max_iter);
-        let index = (i as f32 / max_iter as f32) as usize;
+        let index = (255.0 - i as f32 / max_iter as f32 * 255.0) as usize;
         image::Rgb([(cividis::CIVIDIS[index][0] * 255.0) as u8,
                     (cividis::CIVIDIS[index][1] * 255.0) as u8,
                     (cividis::CIVIDIS[index][2] * 255.0) as u8])
@@ -25,8 +25,8 @@ fn write_image(filename: &str, w: u32, h: u32) {
 fn main() {
     let filename = String::from("test.png");
 
-    let w = 1920;
-    let h = 1080;
+    let w = 2560;
+    let h = 1440;
 
     write_image(&filename, w, h);
 }
